2b60dd321b6d3b0ea9abd4f67900011a4ee3d196
Aadita Sah <aadityasah13@gmail.com>
2025-10-11 17:15:39 +0530

    project setup

---
+++ components/header/Navbar.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import React, { useState } from "react";
+import { ShoppingCart, UserRound, Menu, X } from "lucide-react";
+import { Button } from "../ui/button";
+
+type Props = {};
+
+export default function Navbar({}: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <div className="grid grid-cols-2 md:grid-cols-5 w-full h-[50px] bg-[#FFFD8F] p-2">
+        <div className="flex justify-start items-center gap-2">
+          <button
+            onClick={() => setIsOpen(true)}
+            className="flex md:hidden text-[#043915]"
+          >
+            <Menu className="h-6 w-6" />
+          </button>
+
+          <p className="text-[24px] font-[700] text-[#043915]">Eato</p>
+        </div>
+
+        <div className="hidden md:flex col-span-3 justify-center items-center gap-3">
+          <Button className="bg-transparent hover:bg-transparent text-[#043915] font-[700] text-[18px] hover:underline cursor-pointer">
+            Home
+          </Button>
+          <Button className="bg-transparent hover:bg-transparent text-[#043915] font-[700] text-[18px] hover:underline cursor-pointer">
+            Menu
+          </Button>
+          <Button className="bg-transparent hover:bg-transparent text-[#043915] font-[700] text-[18px] hover:underline cursor-pointer">
+            About
+          </Button>
+        </div>
+
+        <div className="flex justify-end items-center gap-2">
+          <Button className="bg-[#B0CE88] text-[#043915] font-[700] hover:bg-[#B0CE88] cursor-pointer">
+            <ShoppingCart className="mr-1" /> Cart
+          </Button>
+          <Button className="bg-[#B0CE88] text-[#043915] font-[700] hover:bg-[#B0CE88] cursor-pointer">
+            <UserRound className="mr-1" /> Login
+          </Button>
+        </div>
+      </div>
+
+      {/* Mobile Sidebar */}
+      <div
+        className={`fixed top-0 left-0 h-full w-64 bg-[#FFFD8F] text-[#043915] p-5 z-50 transform transition-transform duration-300 ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        {/* Close button */}
+        <button
+          onClick={() => setIsOpen(false)}
+          className="absolute top-4 right-4 text-[#043915]"
+        >
+          <X className="h-6 w-6" />
+        </button>
+
+        {/* Sidebar content */}
+        <div className="mt-10 flex flex-col gap-6">
+          <Button
+            variant="ghost"
+            className="bg-transparent text-[#043915] text-lg font-bold hover:underline"
+          >
+            Home
+          </Button>
+          <Button
+            variant="ghost"
+            className="bg-transparent text-[#043915] text-lg font-bold hover:underline"
+          >
+            Menu
+          </Button>
+          <Button
+            variant="ghost"
+            className="bg-transparent text-[#043915] text-lg font-bold hover:underline"
+          >
+            About
+          </Button>
+        </div>
+      </div>
+
+      {/* Overlay */}
+      {isOpen && (
+        <div
+          onClick={() => setIsOpen(false)}
+          className="fixed inset-0 bg-black/20 bg-opacity-30 z-40"
+        />
+      )}
+    </>
+  );
+}
